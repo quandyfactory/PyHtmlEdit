ad62444f49d9c1209ddf30601112e6e0f3ff60a3
Ryan McGreal <ryan@quandyfactory.com>
2011-03-11 11:12:57 -0800

    Added `try ... except` ImportError around `import wxversion` to catch people who don't have wx installed.

--- pyhtmledit.py
+++ pyhtmledit.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
-__version__ = 0.57
-__releasedate__ = '2011-02-10'
+__version__ = 0.58
+__releasedate__ = '2011-03-11'
 __author__ = 'Ryan McGreal <ryan@quandyfactory.com>'
 __homepage__ = 'http://quandyfactory.com/projects/2/pyhtmledit/'
 __repository__ = 'http://github.com/quandyfactory/PyHtmlEdit'
 __copyright__ = '(C) 2009 by Ryan McGreal. Licenced under GNU GPL 2.0\nhttp://www.gnu.org/licenses/old-licenses/gpl-2.0.html'
 
-import wxversion
-wxversion.select('2.8')
 import sys
+
+try:
+    import wxversion
+except ImportError:
+    print "You must have the wxPython GUI library installed to use this application."
+    sys.exit()
+    
+wxversion.select('2.8')
 import wx
+
 import re
 import os
 import pickle
